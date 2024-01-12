cba252173688e050133efb485615a4f6f695a733
Varad Sharma <varad.code19@gmail.com>
2024-01-12 22:53:46 +0530

    hero progress

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen">
       <img src='/Group_2.svg' className='w-screen h-screen fixed -z-10' />
-      <img src="mangoes.svg" className='-translate-x-[1vw] w-[15rem] h-[15rem]' />
+      <img src="mangoes.svg" className='-translate-x-[1vw] w-[7rem] h-[7rem] md:w-[15rem] md:h-[15rem]' />
+      <img src="/can.svg" className='translate-y-[20vh] translate-x-[65vw] fixed' />
+
+      <div className='flex flex-col h-screen justify-center align-middle z-4'>
+        <h1 className='text-5xl font-bold text-sky-800'>Healthy Human</h1>
+        <p className='text-xl text-sky-800'>Healthy Human is a health food store that sells a variety of products from fruits to vegetables to grains.</p>
+      </div>
+
       {/* {products.map((prod) => { */}
       {/*   return ( */}
       {/*     <ProdCard key={prod.id} prod={prod.name} image={prod.image} /> */}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,10 @@
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Shop</a></li>
-        <li><a>About</a></li>
+    <nav className="fixed z-2">
+      <ul className="hidden md:flex flex-row w-[85vw] text-xl text-sky-800 font-bold justify-end content-end mt-5 mr-5">
+        <li className="px-5"><a href="/">Home</a></li>
+        <li className="px-5"><a href="/shop">Shop</a></li>
+        <li className="px-5"><a href="/cart">Cart</a></li>
       </ul>
     </nav>
   )
