7e4f83c9b89afb067d6d4b402ba6b5b62f8afe77
Varad Sharma <varad.code19@gmail.com>
2024-01-12 23:56:23 +0530

    suka

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ export default function Home() {
       <img src="mangoes.svg" className='-translate-x-[1vw] w-[7rem] h-[7rem] md:w-[15rem] md:h-[15rem]' />
       <img src="/can.svg" className='translate-y-[20vh] translate-x-[65vw] fixed' />
 
-      <div className='flex flex-col h-screen justify-center align-middle z-4'>
-        <h1 className='text-5xl font-bold text-sky-800'>Healthy Human</h1>
-        <p className='text-xl text-sky-800'>Healthy Human is a health food store that sells a variety of products from fruits to vegetables to grains.</p>
+      <div className='flex flex-col h-screen md:justify-center align-middle z-4 p-10'>
+        {/* <h1 className='text-5xl font-bold text-orange-700'>Healthy Human</h1> */}
+        {/* <p className='text-xl text-orange-600'>Healthy Human is a health food store that sells a variety of products from fruits to vegetables to grains.</p> */}
+        <img src="/hero-text.svg" className="h-[50rem] w-[50rem] md:text-3xl md:w-[60rem] md:h-[60rem]" />
       </div>
 
       {/* {products.map((prod) => { */}
