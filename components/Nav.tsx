582a8c2341cf596a19c3fb71f2914b63f2ae4f50
Varad Sharma <varad.code19@gmail.com>
2024-01-13 19:39:39 +0530

    navfix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ const Nav = ({ zIndex }: { zIndex: string }) => {
     <nav className={zIndex}>
       <ul className="hidden md:flex flex-row w-[85vw] text-xl text-sky-800 font-bold justify-end content-end mt-5 mr-5">
         <li className="px-5"><a href="/">Home</a></li>
-        <li className="px-5"><a href="/shop">Shop</a></li>
+        <li className="px-5"><a href="/store">Shop</a></li>
         <li className="px-5"><a href="/cart">Cart</a></li>
       </ul>
     </nav>
