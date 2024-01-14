4914d2bfa17ea33de7cc7ee452e49958fdc7ede1
Varad Sharma <varad.code19@gmail.com>
2024-01-14 13:39:32 +0530

    responsive + berries

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ export default function Home() {
       <img src='/Group_2.svg' className='w-screen h-screen fixed -z-10' />
       <img src="mangoes.svg" className='-translate-x-[1vw] w-[7rem] h-[7rem] md:w-[15rem] md:h-[15rem]' />
       <div className=''>
-        <img src="/can.svg" className='z-20 translate-y-[70vh] -translate-x-[45vw] md:translate-y-[30vh]  md:translate-x-[38vw] 2xl:translate-x-[60vw] fixed' />
+        <img src="/can.svg" className='z-20 translate-y-[66vh] -translate-x-[16vw] md:translate-y-[30vh]  md:translate-x-[38vw] 2xl:translate-x-[60vw] fixed' />
         <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[73vh] md:translate-x-[43vw] 2xl:translate-x-[57vw] fixed' />
         <img src="/mango.svg" className='hidden md:block z-10 translate-y-[45vh] md:translate-x-[60vw] 2xl:translate-x-[75vw] fixed' />
         <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[73vh] md:translate-x-[68vw] 2xl:translate-x-[75vw] fixed' />
         <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[75vh] md:translate-x-[65vw] 2xl:translate-x-[73vw] fixed' />
+        <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[45vh] md:translate-x-[45vw] 2xl:translate-x-[63vw] fixed' />
+        <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[45vh] md:translate-x-[25vw] 2xl:translate-x-[23vw] fixed' />
+        <img src="/blueberry.svg" className='hidden md:block z-10 translate-y-[30vh] md:translate-x-[30vw] 2xl:translate-x-[33vw] fixed' />
       </div>
 
-      <div className='flex flex-col w-screen h-screen justify-center items-center align-middle z-20 -translate-x-[17vw] text-center md:p-10 '>
+      <div className='flex flex-col w-screen h-screen justify-center items-center align-middle z-20 -translate-x-[20vw] text-center md:p-10 '>
         <h1 className='hero-text text-6xl font-extrabold text-yellow-300 uppercase'>Discover the taste of nature{"'"}s <br /> sweetest mangoes</h1>
         {/* <p className='text-xl text-orange-600'>Healthy Human is a health food store that sells a variety of products from fruits to vegetables to grains.</p> */}
         {/* <img src="/hero-text.svg" className="w-[50rem] md:text-3xl md:w-[60rem]" /> */}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,10 @@
 const Nav = ({ zIndex }: { zIndex: string }) => {
   return (
     <nav className={zIndex}>
-      <ul className="hidden md:flex flex-row w-[85vw] text-xl text-sky-800 font-bold justify-end content-end mt-5 mr-5">
-        <li className="px-5"><a href="/">Home</a></li>
-        <li className="px-5"><a href="/store">Shop</a></li>
-        <li className="px-5"><a href="/cart">Cart</a></li>
+      <ul className="flex flex-row w-screen md:w-[85vw] text-xl text-sky-800 font-bold justify-end content-end mt-5 mr-6">
+        <li className="px-3 md:px-5"><a href="/">Home</a></li>
+        <li className="px-3 md:px-5"><a href="/store">Shop</a></li>
+        <li className="px-3 md:px-5"><a href="/cart">Cart</a></li>
       </ul>
     </nav>
   )
